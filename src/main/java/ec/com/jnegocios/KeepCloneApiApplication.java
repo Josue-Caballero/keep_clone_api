f4625e4807631a25d4afdd446e8bc0f273b6e92c
Jorge Diaz Montoya <jdzm@outlook.es>
2020-04-23 13:42:32 -0500

    Start project and setup dependencies

---
+++ src/main/java/ec/com/jnegocios/KeepCloneApiApplication.java
@@ -0,0 +1,13 @@
+package ec.com.jnegocios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepCloneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepCloneApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/com/jnegocios/KeepCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.jnegocios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepCloneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
